ea35f8bba32b2937975daa2f31be3a1a0625711c
Geoffrey Schwartz <geoff@iCow.local>
2010-12-04 17:00:58 -0500

    the schema file

---
+++ schema.sql
@@ -0,0 +1,16 @@
+create table organization
+(
+    organization_id unsigned int,
+    name text,
+    type text,
+    note text,
+    size text,
+    description text,
+    feed_url text,
+    qr_url text,
+    url text,
+);
+
+create table operation
+(
+);

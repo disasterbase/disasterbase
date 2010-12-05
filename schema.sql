2f93e0f38dd56150c507a7b3598ee31c8f208d92
Geoffrey Schwartz <geoff@iCow.local>
2010-12-05 13:11:35 -0500

    media tables? hopefully? and sample data taken from juan's JSON

--- schema.sql
+++ schema.sql
@@ -11,9 +11,10 @@ drop table if exists disaster;
 
 create table media
 (
-    media_id    int unsigned not null auto_increment primary key,
-    media_type  varchar(255),
-    media_url   text
+    media_id        int unsigned not null auto_increment primary key,
+    media_type      varchar(255),
+    media_url       text,
+    media_caption   text
 );
 
 create table user 
@@ -29,7 +30,7 @@ create table user
 create table organization
 (
     organization_id int unsigned not null auto_increment primary key,
-	approved tinyint,
+	approved        tinyint,
     name            text,
     type            text,
     note            text,
@@ -59,7 +60,7 @@ create table organization_contact
 create table project
 (
     project_id      int unsigned not null auto_increment primary key,
-	approved tinyint,
+    approved        tinyint,
     disaster_id     int unsigned not null,
     name            text,
     type            text,
@@ -95,14 +96,143 @@ create table organization_project_media
 create table disaster
 (
     disaster_id         int unsigned not null auto_increment primary key,
-	approved tinyint,
+	approved            tinyint,
     location_lat        text,
     location_lon        text,
     name                text,
     area_name           text,
     area_abbreviation   text,
+    date_start          date,
     type                text,
     population_affected int unsigned,
     severity            text,
+    funded              enum('yes', 'no'),
     lending_type        enum('none', 'IDA', 'BBDV')
 );
+
+--------------------------------------------------------------------------------
+
+-- disaster
+insert into disaster
+set
+    location_lat        = "19.15",
+    location_lon        = "-72.29",
+    name                = "Haiti",
+    area_name           = "Latin America & Caribbean",
+    area_abbreviation   = "LAC",
+    date_start          = "2010-12-01",
+    type                = "hurricane",
+    population_affected = "100000",
+    severity            = "high",
+    funded              = "yes",
+    lending_type        = "IDA";
+
+set @disaster_id = last_insert_id();
+
+-- project
+insert into project
+set
+    disaster_id     = @disaster_id,
+    name            = "Some project name",
+    type            = "Rebuilding Building",
+    description     = "This is a rather long description.",
+    date_start      = "2010-01-01",
+    funded          = "yes",
+    lending_type    = "IDA";
+
+set @project_id = last_insert_id();
+
+-- organization 1
+insert into organization
+set
+    name        = "Red Cross",
+    type        = "NGO",
+    note        = "",
+    size        = "Large",
+    description = "Something rather nice",
+    logo_url    = "http://www.redcross.org/files/site/images/logo.gif",
+    qr_url      = "http://www.the2dcode.com/qr-code/120994",
+    url         = "http://www.redcross.org";
+
+set @organization_id = last_insert_id();
+
+insert into organization_contact
+set
+    organization_id = @organization_id,
+    contact_type    = "twitter",
+    contact_name    = "Juan Muller",
+    contact_info    = "http://twitter.com/juancmuller";
+
+insert into organization_project
+set
+    organization_id = @organization_id,
+    project_id      = @project_id,
+    feed_url        = "http://twitter.com/favorites/13907922.rss";
+
+-- organization 2
+insert into organization
+set
+    name        = "Red Cross",
+    type        = "NGO",
+    note        = "",
+    size        = "Large",
+    description = "Something rather nice",
+    logo_url    = "http://www.redcross.org/files/site/images/logo.gif",
+    qr_url      = "http://www.the2dcode.com/qr-code/120994",
+    url         = "http://www.redcross.org";
+
+set @organization_id = last_insert_id();
+
+insert into organization_contact
+set
+    organization_id = @organization_id,
+    contact_type    = "twitter",
+    contact_name    = "Juan Muller",
+    contact_info    = "http://twitter.com/juancmuller";
+
+insert into organization_project
+set
+    organization_id = @organization_id,
+    project_id      = @project_id,
+    feed_url        = "http://twitter.com/favorites/13907922.rss";
+
+-- media
+insert into media
+set
+    media_type      = "image/jpeg",
+    media_url       = "http://www.amigosdeperu.org/STORE/haiti-disaster-2010.jpg",
+    media_caption   = "People doing something or other.";
+
+set @media_id = last_insert_id();
+
+insert into media
+set
+    media_type      = "image/jpeg",
+    media_url       = "http://frontiersfoundation.ca/system/files/images/DSC04685.preview.JPG",
+    media_caption   = "People doing something or other.";
+
+set @media_id = last_insert_id();
+
+insert into media
+set
+    media_type      = "image/jpeg",
+    media_url       = "http://cdn.necolebitchie.com/wp-content/uploads/2010/01/haiti-disaster-2.jpg",
+    media_caption   = "People doing something or other.";
+
+set @media_id = last_insert_id();
+
+insert into media
+set
+    media_type      = "image/jpeg",
+    media_url       = "http://media.komonews.com/images/100117_haiti_disaster.jpg",
+    media_caption   = "";
+
+set @media_id = last_insert_id();
+
+insert into media
+set
+    media_type      = "image/jpeg",
+    media_url       = "http://www.about-knowledge.com/wp-content/uploads/2010/01/haiti-disaster-relief.jpg",
+    media_caption   = "";
+
+set @media_id = last_insert_id();

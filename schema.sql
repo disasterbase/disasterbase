d1b43730282ff885383072de2a0cbda4bf07f6c7
Geoffrey Schwartz <geoff@iCow.local>
2010-12-04 21:18:28 -0500

    added media table and associated meta stuff

--- schema.sql
+++ schema.sql
@@ -1,53 +1,97 @@
+drop table user;
+drop table media;
+drop table organization;
+drop table organization_media;
+drop table organization_contact;
+drop table project;
+drop table project_media;
+drop table organization_project;
+drop table organization_project_media;
+drop table disaster;
+
+create table user
+(
+    user_id     int unsigned not null auto_increment primary key,
+    username    varchar(255),
+    password    varchar(255)
+);
+
+create table media
+(
+    media_id    int unsigned not null auto_increment primary key,
+    media_type  varchar(255),
+    media_url   text
+);
+
 create table organization
 (
     organization_id int unsigned not null auto_increment primary key,
-    name text,
-    type text,
-    note text,
-    size text,
-    description text,
-    logo_url text,
-    qr_url text,
-    url text
+    name            text,
+    type            text,
+    note            text,
+    size            text,
+    description     text,
+    logo_url        text,
+    qr_url          text,
+    url             text
+);
+
+create table organization_media
+(
+    organization_id int unsigned,
+    media_id        int unsigned
 );
 
 create table organization_contact
 (
     organization_id int unsigned,
-    contact_type enum('email', 'phone', 'mobile', 'website', 'skype', 'twitter'),
-    contact_info text
+    contact_type    enum('email', 'phone', 'mobile', 'website', 'skype', 'twitter'),
+    contact_info    text
 );
 
 create table project
 (
-    project_id int unsigned not null auto_increment primary key,
-    disaster int unsigned not null,
-    name text,
-    type text,
-    description text,
-    date_start date,
-    date_end date,
-    funded enum('yes', 'no'),
-    lending_type text
+    project_id      int unsigned not null auto_increment primary key,
+    disaster        int unsigned not null,
+    name            text,
+    type            text,
+    description     text,
+    date_start      date,
+    date_end        date,
+    funded          enum('yes', 'no'),
+    lending_type    text
+);
+
+create table project_media
+(
+    project_id  int unsigned,
+    media_id    int unsigned
 );
 
 create table organization_project
 (
-    organization_id int unsigned,
-    project_id int unsigned,
-    feed_url text
+    organization_project_id int unsigned not null auto_increment primary key,
+    organization_id         int unsigned,
+    project_id              int unsigned,
+    feed_url                text
+);
+
+create table organization_project_media
+(
+    organization_project_id int unsigned,
+    media_id                int unsigned
 );
 
 create table disaster
 (
-    disaster_id int unsigned not null auto_increment primary key,
-    location_lat text,
-    location_lon text,
-    name text,
-    area_name text,
-    area_abbreviation text,
-    type text,
+    disaster_id         int unsigned not null auto_increment primary key,
+    location_lat        text,
+    location_lon        text,
+    name                text,
+    area_name           text,
+    area_abbreviation   text,
+    type                text,
     population_affected int unsigned,
-    severity text,
-    lending_type enum('none', 'IDA', 'BBDV')
+    severity            text,
+    lending_type        enum('none', 'IDA', 'BBDV')
 );

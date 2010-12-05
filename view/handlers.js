ecd2ba6ffce77ebe5af6ede6f00fe33fb038e68f
Juan C. Muller <jcmuller@gmail.com>
2010-12-05 13:48:12 -0500

    This might be just it.

--- view/handlers.js
+++ view/handlers.js
@@ -26,18 +26,34 @@ var clean_all = function ()
 	clean_footer();
 };
 
-var render_results = function (data, textStatus, req) {
+var successful_request = function (data, textStatus, req)
+{
 	disasters = data.disasters;
+	render_results(disasters);
+}
 
+var render_results = function (dis)
+{
 	var head_info = $("#head_info");
 	var head_map  = $("#head_map");
 	var col1      = $("#col1");
 	var col2      = $("#col2");
 	var footer    = $("#footer");
 
+	clean_all();
+
 	var template  = _.template($("#header_template").html());
 
-	$.each(disasters, function () {
+	var ds;
+	if (dis)
+		if (dis.disasters)
+			console.log(dis.disasters);
+		else
+			console.log(dis);
+
+	console.log(dis);
+
+	$.each(dis.disasters, function () {
 		var today = new Date();
 		var start = new Date(this.disaster.date_start);
 		this.disaster.days = parseInt((today - start) / 1000 / 60 / 60 / 24);
@@ -47,8 +63,7 @@ var render_results = function (data, textStatus, req) {
 			var organizations = this.organizations;
 			template = _.template($("#organization_logo").html());
 			$.each(organizations, function () {
-				for (var i = 0; i < 10; i++)
-					col1.append(template(this));
+				col1.append(template(this));
 			});
 
 			_.delay(show_organizations, 1000 * timeout, organizations);
@@ -77,16 +92,16 @@ var show_organizations = function (organizations)
 	var len = organizations.length
 	render_pager(len);
 	var i = 0;
-	while (i < len)
-	{
-		_.delay(show_organization, 1000 * timeout, organizations[i]);
-		i++;
-	}
+
+	console.log("Show organzation");
+	show_organization(organizations, i, len);
+	console.log("Finishing iteration");
 };
 
-var show_organization = function (organization, i)
+var show_organization = function (organizations, i, all)
 {
 	clean_main();
+	var organization = organizations[i];
 	$(".page").css({ 'color': 'black'});
 	$("#page_" + i).css({ 'color': 'red'});
 	var template = _.template($("#organization_details").html());
@@ -95,12 +110,31 @@ var show_organization = function (organization, i)
 
 	$("#col2").empty();
 	template = _.template($("#feed_entry").html());
+
+	i++;
+	if (i == all)
+		_.delay(do_magical_things, 1000 * timeout);
+	else
+		_.delay(show_organization, 1000 * timeout, organizations, i, all);
 }
 
 var display_tweets = function (tweets)
 {
 	var template = _.template($("#feed_entry").html());
 	$.each(tweets, function () {
+		var today = new Date();
+		var then = new Date(this.created_at);
+
+		var days = parseInt((today - then) / 1000 / 60 / 60 / 24);
+		if (days == 0)
+			days = 'today';
+		else if (days == 1)
+			days = days + ' day ago';
+		else
+			days = days + ' days ago';
+
+		this.days = days;
+
 		$('#col2').append(template(this));
 	});
 }
@@ -112,8 +146,8 @@ var render_pager = function (count)
 		$("div#footer").append(template({ "counter": i }));
 };
 
-// Entry point
-$(document).ready(function () {
+do_magical_things = function ()
+{
 	$.ajax({
 		url: 'fake_data.json',
 		cache: false,
@@ -121,4 +155,7 @@ $(document).ready(function () {
 		data: {},
 		success: render_results
 	});
-});
+}
+
+// Entry point
+$(document).ready(do_magical_things);
